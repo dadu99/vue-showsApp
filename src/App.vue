ebf13c5c15cd8b2253816a169b2cc5cdd6fe5440
Darius Baciu <baciu.darius01@gmail.com>
2023-11-29 11:26:48 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import RickAndMortyCards from "./components/RickAndMorty/RickAndMortyCards.vue";
+
+</script>
+
+
+<template>
+  <main>
+    <h1>Hero</h1>
+    <Suspense>
+      <template #default>
+        <RickAndMortyCards></RickAndMortyCards>
+      </template>
+      <template #fallback>
+        <div>
+          <p>Loading..</p>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/RickAndMorty/RickAndMortyCards.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from "axios"
+import { ref } from 'vue';
+
+const response = await axios.get("https://rickandmortyapi.com/api/character");
+console.log(response);
+
+
+
+
+</script>
+
+
+<template>
+    <div>
+        <h1>Rick and morty cards</h1>
+        <p>{{ response }}</p>
+    </div>
+</template>
